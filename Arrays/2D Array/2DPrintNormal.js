9045e1c213e97785c9da9b67e192c94bdd5bc4b8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-07-02 10:29:37 +0530

    Create 2DPrintNormal.js

---
+++ Arrays/2D Array/2DPrintNormal.js
@@ -0,0 +1,17 @@
+let arr=[
+//    0     1
+  ["Yash", "Ok"],
+  ["No", "Thanks"]
+]
+
+console.log(arr[0][0]);
+console.log(arr[0][1]);
+console.log(arr[1][0]);
+console.log(arr[1][1]);
+
+// Output:
+
+// Yash      [0][0]
+// Ok        [0][1]
+// No        [1][0]
+// Thanks    [1][1]
