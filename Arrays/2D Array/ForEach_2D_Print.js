01c5808d3a92ff3bddc648ce64f7c0c53ece2461
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-07-02 10:06:19 +0530

    Create ForEach_2D_Print.js

---
+++ Arrays/2D Array/ForEach_2D_Print.js
@@ -0,0 +1,22 @@
+let arr=[
+    ["Yash",30],
+    ["Thomas",28],
+    ["Robeert",30]
+  ]
+  
+  arr.forEach(data =>
+  {
+    data.forEach(val=>
+    {
+      console.log(val);
+    })
+  });
+
+// Output:
+
+// Yash
+// 30
+// Thomas
+// 28
+// Robeert
+// 30
