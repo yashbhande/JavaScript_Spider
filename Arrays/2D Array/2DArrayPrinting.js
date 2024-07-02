2117ed33e65ee00714141761acb27f4b78f845c4
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-07-02 10:02:48 +0530

    Create 2DArrayPrinting.js

---
+++ Arrays/2D Array/2DArrayPrinting.js
@@ -0,0 +1,21 @@
+let arr=[
+    ["John",30],
+    ["Thomas",28],
+    ["Robeert",30]
+  ]
+  
+  // console.log(arr[1][0]);
+  for(let i=0; i<arr.length; i++)
+  {
+    for(let j=0; j<arr[i].length; j++)
+    console.log(arr[i][j]);
+  }
+
+Output:
+
+John
+30
+Thomas
+28
+Robeert
+30
